3052b2becc8304991d0871e27be02d23da582e83
Luiz Pegoraro <pegorarohluiz@gmail.comw>
2023-12-22 10:08:57 -0300

    feat(httpmetrics): rename packages.

--- receiver/httpmetricsreceiver/config.go
+++ receiver/httpmetricsreceiver/config.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package httpcheckreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/httpcheckreceiver"
+package httpmetricsreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/httpcheckreceiver"
 
 import (
 	"errors"
