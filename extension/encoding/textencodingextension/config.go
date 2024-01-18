00d44c4867dcd3769a97fdfb370e0752bd47d63b
Luiz Pegoraro <pegorarohluiz@gmail.comw>
2024-01-18 14:46:22 -0300

    feat(build): clean up.

--- extension/encoding/textencodingextension/config.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-package textencodingextension // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/encoding/textencodingextension"
-
-type Config struct {
-	Encoding string `mapstructure:"encoding"`
-}
